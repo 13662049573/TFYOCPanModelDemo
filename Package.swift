d80b9cc7cab40f9a2e56cbe819512714b86c118d
tianfengyou <420144542@qq.com>
2025-11-22 08:40:46 +0800

    fix: 移除 Package.swift 中不存在的文件引用

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
             dependencies: [],
             path: "TFYOCPanModelDemo/TFYOCPanlModel",
             exclude: [
-                "module.modulemap.cocoapods",
                 "TFYOCPanlModel.h"
             ],
             publicHeadersPath: "include",
