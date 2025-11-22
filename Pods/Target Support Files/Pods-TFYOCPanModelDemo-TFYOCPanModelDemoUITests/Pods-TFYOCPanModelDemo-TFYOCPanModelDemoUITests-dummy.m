666230da705038955fd6c2ee359de77e783bcbc9
tianfengyou <420144542@qq.com>
2025-11-22 10:29:42 +0800

    修复

--- Pods/Target Support Files/Pods-TFYOCPanModelDemo-TFYOCPanModelDemoUITests/Pods-TFYOCPanModelDemo-TFYOCPanModelDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TFYOCPanModelDemo_TFYOCPanModelDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TFYOCPanModelDemo_TFYOCPanModelDemoUITests
-@end
--- Pods/Target Support Files/Pods-TFYOCPanModelDemoTests/Pods-TFYOCPanModelDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TFYOCPanModelDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_TFYOCPanModelDemoTests
-@end
