f99f843ac6cdceaba4da0386b5ef121f3c2b21b5
tianfengyou <420144542@qq.com>
2025-11-22 09:09:58 +0800

    修复Swift Package：设置静态库类型，创建.swiftpm/ignore隐藏非库文件，修复模块导入问题，版本更新至1.5.5

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "TFYOCPanlModel",
+            type: .static,
             targets: ["TFYOCPanlModel"]
         ),
     ],
