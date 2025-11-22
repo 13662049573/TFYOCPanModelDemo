4810ab256ac648443202e5fbdf78e9ce858d69b8
tianfengyou <420144542@qq.com>
2025-11-22 11:22:57 +0800

    修复问题

--- Package.swift
+++ Package.swift
@@ -1,46 +1,33 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "TFYOCPanlModel",
-    platforms: [
-        .iOS(.v15)
-    ],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "TFYOCPanlModel",
             type: .static,
             targets: ["TFYOCPanlModel"]
-        ),
-    ],
-    dependencies: [
-        // No external dependencies - this library only uses UIKit and Foundation
+        )
     ],
     targets: [
         .target(
             name: "TFYOCPanlModel",
-            dependencies: [],
             path: "TFYOCPanModelDemo/TFYOCPanlModel",
             sources: [
                 "Tools",
-                "popController",
+                "popController", 
                 "popView",
-                "include",
+                "include"
             ],
+            publicHeadersPath: "include",        // 关键：告诉 SPM 公共头文件在 include
             cSettings: [
-                .headerSearchPath("."),
                 .headerSearchPath("include"),
                 .headerSearchPath("Tools"),
                 .headerSearchPath("popController"),
-                .headerSearchPath("popView"),
-            ],
-            linkerSettings: [
-                .linkedFramework("UIKit", .when(platforms: [.iOS])),
-                .linkedFramework("Foundation", .when(platforms: [.iOS])),
+                .headerSearchPath("popView")
             ]
-        ),
-    ],
-    cLanguageStandard: .c11
+        )
+    ]
 )
