54948ce9ac5ed6b6950356b4838f73894afc9357
tianfengyou <420144542@qq.com>
2025-11-22 12:56:09 +0800

    chore: 更新版本至 1.6.2
    - 版本更新至 1.6.2

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.9
-// 注意：这一行必须干净，不能有任何注释或空格在版本号后面！！！
-
 import PackageDescription
 
 let package = Package(
@@ -23,23 +21,11 @@ let package = Package(
                 "popController",
                 "popView"
             ],
-            publicHeadersPath: "include",
-            cSettings: [
-                // 常规搜索路径
-                .headerSearchPath(""),
-                .headerSearchPath("include"),
-                .headerSearchPath("Tools"),
-                .headerSearchPath("popController"),
-                .headerSearchPath("popView"),
-
-                // 终极杀招：用纯相对路径 -I，彻底绕过 Xcode 对变量的限制
-                .unsafeFlags(["-I", "."]),
-                .unsafeFlags(["-I", "TFYOCPanModelDemo/TFYOCPanlModel"]),
-                .unsafeFlags(["-I", "TFYOCPanModelDemo/TFYOCPanlModel/include"]),
-                .unsafeFlags(["-I", "TFYOCPanModelDemo/TFYOCPanlModel/Tools"]),
-                .unsafeFlags(["-I", "TFYOCPanModelDemo/TFYOCPanlModel/popController"]),
-                .unsafeFlags(["-I", "TFYOCPanModelDemo/TFYOCPanlModel/popView"])
-            ]
+            // 关键三行：彻底关闭 Objective-C module 构建
+            publicHeadersPath: nil,   // 1. 不生成模块头文件映射
+            cSettings: nil,           // 2. 不添加任何 cSettings
+            linkerSettings: nil       // 3. 关闭 linker 设定
+            // 注意：没有 type 参数！这就是导致报错的原因
         )
     ]
 )
